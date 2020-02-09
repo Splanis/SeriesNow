70b093fe6246996fc22123b54165204073052c99
Marios G <splanis5@gmail.com>
2020-02-09 18:54:57 +0200

    Footer to postition static

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -2,16 +2,13 @@ import React from "react";
 import styled from "styled-components";
 
 const Footer = () => {
-    return (
-        <Foot>Footer</Foot>
-    );
-}
+    return <FooterContainer>Footer</FooterContainer>;
+};
 
-const Foot = styled.nav`
+const FooterContainer = styled.nav`
     background: black;
     color: white;
     height: 60px;
-    position: fixed;
     width: 100%;
     display: flex;
     align-items: center;
