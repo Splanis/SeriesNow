6eef92b3646ac2f02b366132a7ddcb8ecd6a6306
Marios G <splanis5@gmail.com>
2020-02-10 22:52:06 +0200

    removed Fetch from navbar

--- src/components/layout/Navbar.tsx
+++ src/components/layout/Navbar.tsx
@@ -1,47 +1,13 @@
-import React, { useState } from "react";
+import React from "react";
 import styled from "styled-components";
 import { Link } from "react-router-dom";
-import { IMovie, useMovies } from "../context/MoviesContext";
-
-const API_KEY = `98b9ebfd32ac53d37febef32464f8607`;
-
-// const API_URL = `https://api.themoviedb.org/3/search/movie?api_key=${API_KEY}&language=en-US&query=${query}&page=1&include_adult=false`;
+import { useShows } from "../context/ShowContext";
 
 const Movies: React.FC = () => {
-    const providerValues = useMovies();
-    const [query, setQuery] = useState<string>();
+    const providerValues = useShows();
 
-    const queryHandle = async (e: React.ChangeEvent<HTMLInputElement>) => {
-        setQuery(e.target.value);
-        if (query) {
-            searchFetch();
-        }
-    };
-
-    const searchFetch = async () => {
-        const response = await fetch(
-            `https://api.themoviedb.org/3/search/movie?api_key=${API_KEY}&language=en-US&query=${query}&page=1&include_adult=false&region=en`
-        );
-        const data = await response.json();
-        console.log(data.results);
-        providerValues?.setMovies(
-            data.results.map((movie: IMovie) => ({
-                adult: movie.adult,
-                backdrop_path: movie.backdrop_path,
-                title: movie.title,
-                id: movie.id,
-                popularity: movie.popularity,
-                vote_count: movie.vote_count,
-                video: movie.video,
-                poster_path: movie.poster_path,
-                original_language: movie.original_language,
-                original_title: movie.original_title,
-                genre_ids: movie.genre_ids,
-                vote_average: movie.vote_average,
-                overview: movie.overview,
-                release_date: movie.release_date
-            }))
-        );
+    const queryHandle = (e: React.ChangeEvent<HTMLInputElement>) => {
+        providerValues?.setQuery(e.target.value);
     };
 
     return (
@@ -49,12 +15,16 @@ const Movies: React.FC = () => {
             <StyledLink to="/">Logo</StyledLink>
 
             <form action="">
-                <input type="text" value={query} placeholder="search" onChange={queryHandle} />
+                <input type="text" value={providerValues?.query} placeholder="search" onChange={queryHandle} />
             </form>
 
             <div>
-                <StyledLink to="/series">Series</StyledLink>
-                <StyledLink to="/movies">Movies</StyledLink>
+                <StyledLink to="/series" onClick={() => {providerValues?.setShow('tv')}}>
+                    Series
+                </StyledLink>
+                <StyledLink to="/movies" onClick={() => {providerValues?.setShow('movie')}}>
+                    Movies
+                </StyledLink>
             </div>
         </Nav>
     );
@@ -74,7 +44,9 @@ const Nav = styled.nav`
 
 const StyledLink = styled(Link)`
     color: white;
-    margin: 10px;
+    margin: 20px;
+    text-decoration: none;
+    font-size: 1.5rem;
 `;
 
 export default Movies;
