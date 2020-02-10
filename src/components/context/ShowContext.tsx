1a54abc908c8ea44abd513dee6862ba0ecc13ba3
Marios G <splanis5@gmail.com>
2020-02-10 22:52:15 +0200

    movies rename to shows

--- src/components/context/ShowContext.tsx
+++ src/components/context/ShowContext.tsx
@@ -22,8 +22,8 @@ export interface IShow {
 }
 
 export interface IShowProviderProps {
-    movies: IShow[];
-    setMovies: (movies: IShow[]) => void;
+    shows: IShow[];
+    setShows: (movies: IShow[]) => void;
     sort: string;
     setSort: (sort: string) => void;
     query: string;
@@ -35,19 +35,19 @@ export interface IShowProviderProps {
 export const ShowContext = createContext<IShowProviderProps | null>(null);
 
 export const ShowProvider = ({ children }: Props): JSX.Element => {
-    const [movies, setMovies] = useState<IShow[]>([]);
+    const [shows, setShows] = useState<IShow[]>([]);
     const [sort, setSort] = useState<string>("popularity.desc");
     const [query, setQuery] = useState<string>("");
-    const [show, setShow] = useState<'movie' | 'tv'>("movie");
+    const [show, setShow] = useState<'movie' | 'tv'>("tv");
 
     const API_KEY = `98b9ebfd32ac53d37febef32464f8607`;
     const API_URL = `https://api.themoviedb.org/3/discover/${show}?api_key=${API_KEY}&language=en-US&sort_by=${sort}&include_adult=false&include_video=false&page=1`;
     const API_QUERY_URL = `https://api.themoviedb.org/3/search/${show}?api_key=${API_KEY}&language=en-US&query=${query}&page=1&include_adult=false`;
     let FETCH_URL = "";
-
+   
     const providerValue: IShowProviderProps = {
-        movies,
-        setMovies,
+        shows,
+        setShows,
         sort,
         setSort,
         query,
@@ -64,29 +64,29 @@ export const ShowProvider = ({ children }: Props): JSX.Element => {
         }
         const response = await fetch(FETCH_URL);
         const data = await response.json();
-        setMovies(
-            data.results.map((movie: IShow) => ({
-                adult: movie.adult,
-                backdrop_path: movie.backdrop_path,
-                title: movie.title,
-                id: movie.id,
-                popularity: movie.popularity,
-                vote_count: movie.vote_count,
-                video: movie.video,
-                poster_path: movie.poster_path,
-                original_language: movie.original_language,
-                original_title: movie.original_title,
-                genre_ids: movie.genre_ids,
-                vote_average: movie.vote_average,
-                overview: movie.overview,
-                release_date: movie.release_date
+        setShows(
+            data.results.map((show: IShow) => ({
+                adult: show.adult,
+                backdrop_path: show.backdrop_path,
+                title: show.title,
+                id: show.id,
+                popularity: show.popularity,
+                vote_count: show.vote_count,
+                video: show.video,
+                poster_path: show.poster_path,
+                original_language: show.original_language,
+                original_title: show.original_title,
+                genre_ids: show.genre_ids,
+                vote_average: show.vote_average,
+                overview: show.overview,
+                release_date: show.release_date
             }))
         );
     };
 
     useEffect(() => {
         fetchData();
-    }, [sort, query]);
+    }, [sort, query, show]);
 
     return <ShowContext.Provider value={providerValue}>{children}</ShowContext.Provider>;
 };
