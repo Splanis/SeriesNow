490f7114e4eeb866e784b1d0da3144bacbd3970a
Marios G <splanis5@gmail.com>
2020-02-11 02:48:48 +0200

    Added deafult button style

---
+++ src/components/sharedStyles/Button.tsx
@@ -0,0 +1,15 @@
+import styled from "styled-components";
+
+export const Button = styled.button`
+    padding: 10px;
+    margin: 5px;
+    border: none;
+    border-radius: 10px;
+    background: #000;
+    color: white;
+
+    &:hover {
+        cursor: pointer;
+        background-color: #333333;
+    }
+`;
