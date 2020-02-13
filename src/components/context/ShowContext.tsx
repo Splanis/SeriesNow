2464dbf068f0032b500d6305fa739f742d85277b
Marios G <splanis5@gmail.com>
2020-02-13 03:06:18 +0200

    Context updated

--- src/components/context/ShowContext.tsx
+++ src/components/context/ShowContext.tsx
@@ -24,7 +24,7 @@ export interface IShow {
 
 export interface IShowProviderProps {
     shows: IShow[];
-    setShows: (currentShows: IShow[], shows: IShow[]) => void;
+    setShows: (shows: IShow[]) => void;
     sort: string;
     setSort: (sort: string) => void;
     query: string;
@@ -35,7 +35,7 @@ export interface IShowProviderProps {
     setPage: (page: number) => void;
 }
 
-export const ShowContext = createContext<IShowProviderProps | null>(null);
+export const ShowContext = createContext<IShowProviderProps>({} as IShowProviderProps);
 
 export const ShowProvider = ({ children }: Props): JSX.Element => {
     const [shows, setShows] = useState<IShow[]>([]);
@@ -46,10 +46,10 @@ export const ShowProvider = ({ children }: Props): JSX.Element => {
 
     const API_KEY = `98b9ebfd32ac53d37febef32464f8607`;
     const API_URL = `https://api.themoviedb.org/3/discover/${showType}?api_key=${API_KEY}&language=en-US&sort_by=${sort}&include_adult=false&include_video=false&page=${page}`;
-    const API_SEARCH_URL = `https://api.themoviedb.org/3/search/${showType}?api_key=${API_KEY}&language=en-US&query=${query}&include_adult=false`;
+    const API_SEARCH_URL = `https://api.themoviedb.org/3/search/${showType}?api_key=${API_KEY}&language=en-US&query=${query}&include_adult=false&page=${page}`;
     let FETCH_URL = "";
 
-    const providerValue: IShowProviderProps = {
+    const providerValues: IShowProviderProps = {
         shows,
         setShows,
         sort,
@@ -70,56 +70,48 @@ export const ShowProvider = ({ children }: Props): JSX.Element => {
         }
         const response = await fetch(FETCH_URL);
         const data = await response.json();
+        const showsData = data.results.map((show: IShow) => ({
+            adult: show.adult,
+            backdrop_path: show.backdrop_path,
+            title: show.title,
+            id: show.id,
+            popularity: show.popularity,
+            vote_count: show.vote_count,
+            video: show.video,
+            poster_path: show.poster_path,
+            original_language: show.original_language,
+            original_name: show.original_name,
+            genre_ids: show.genre_ids,
+            vote_average: show.vote_average,
+            overview: show.overview,
+            release_date: show.release_date,
+            first_air_date: show.first_air_date
+        }));
 
-        if (page !== 1) {
-            setShows([
-                ...shows,
-                ...data.results.map((show: IShow) => ({
-                    adult: show.adult,
-                    backdrop_path: show.backdrop_path,
-                    title: show.title,
-                    id: show.id,
-                    popularity: show.popularity,
-                    vote_count: show.vote_count,
-                    video: show.video,
-                    poster_path: show.poster_path,
-                    original_language: show.original_language,
-                    original_name: show.original_name,
-                    genre_ids: show.genre_ids,
-                    vote_average: show.vote_average,
-                    overview: show.overview,
-                    release_date: show.release_date,
-                    first_air_date: show.first_air_date
-                }))
-            ]);
+        if (page === 1) {    
+            if (query) {
+                setShows([...shows, ...showsData]);
+            } else {
+                setShows(showsData);
+            }
         } else {
-            setShows(
-                data.results.map((show: IShow) => ({
-                    adult: show.adult,
-                    backdrop_path: show.backdrop_path,
-                    title: show.title,
-                    id: show.id,
-                    popularity: show.popularity,
-                    vote_count: show.vote_count,
-                    video: show.video,
-                    poster_path: show.poster_path,
-                    original_language: show.original_language,
-                    original_name: show.original_name,
-                    genre_ids: show.genre_ids,
-                    vote_average: show.vote_average,
-                    overview: show.overview,
-                    release_date: show.release_date,
-                    first_air_date: show.first_air_date
-                }))
-            );
+            if (query) {
+              
+                setShows(showsData);
+            } else {
+                setShows([...shows, ...showsData]);
+            }
         }
     };
+    console.log(query);
+    console.log(shows);
+    console.log(page);
 
     useEffect(() => {
         fetchData();
     }, [sort, query, showType, page]);
 
-    return <ShowContext.Provider value={providerValue}>{children}</ShowContext.Provider>;
+    return <ShowContext.Provider value={providerValues}>{children}</ShowContext.Provider>;
 };
 
 export const useShows = () => useContext(ShowContext);
