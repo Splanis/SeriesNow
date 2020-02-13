af8d53a80a622ecaca71f0daa104356bb7771d6d
Marios G <splanis5@gmail.com>
2020-02-13 23:04:06 +0200

    Removed Fetching

--- src/components/context/ShowContext.tsx
+++ src/components/context/ShowContext.tsx
@@ -1,4 +1,4 @@
-import React, { useState, createContext, ReactNode, useContext, useEffect } from "react";
+import React, { useState, createContext, ReactNode, useContext } from "react";
 
 interface Props {
     children: ReactNode;
@@ -22,7 +22,7 @@ export interface IShow {
     first_air_date: string;
 }
 
-export interface IShowProviderProps {
+interface IShowProviderProps {
     shows: IShow[];
     setShows: (shows: IShow[]) => void;
     sort: string;
@@ -38,16 +38,11 @@ export interface IShowProviderProps {
 export const ShowContext = createContext<IShowProviderProps>({} as IShowProviderProps);
 
 export const ShowProvider = ({ children }: Props): JSX.Element => {
-    const [shows, setShows] = useState<IShow[]>([]);
+    const [shows, setShows] = useState<IShow[]>([] as IShow[]);
     const [sort, setSort] = useState<string>("popularity.desc");
     const [query, setQuery] = useState<string>("");
     const [showType, setShowType] = useState<"movie" | "tv">("movie");
     const [page, setPage] = useState<number>(1);
-
-    const API_KEY = `98b9ebfd32ac53d37febef32464f8607`;
-    const API_URL = `https://api.themoviedb.org/3/discover/${showType}?api_key=${API_KEY}&language=en-US&sort_by=${sort}&include_adult=false&include_video=false&page=${page}`;
-    const API_SEARCH_URL = `https://api.themoviedb.org/3/search/${showType}?api_key=${API_KEY}&language=en-US&query=${query}&include_adult=false&page=${page}`;
-    let FETCH_URL = "";
 
     const providerValues: IShowProviderProps = {
         shows,
@@ -61,55 +56,6 @@ export const ShowProvider = ({ children }: Props): JSX.Element => {
         page,
         setPage
     };
-
-    const fetchData = async () => {
-        if (query) {
-            FETCH_URL = API_SEARCH_URL;
-        } else {
-            FETCH_URL = API_URL;
-        }
-        const response = await fetch(FETCH_URL);
-        const data = await response.json();
-        const showsData = data.results.map((show: IShow) => ({
-            adult: show.adult,
-            backdrop_path: show.backdrop_path,
-            title: show.title,
-            id: show.id,
-            popularity: show.popularity,
-            vote_count: show.vote_count,
-            video: show.video,
-            poster_path: show.poster_path,
-            original_language: show.original_language,
-            original_name: show.original_name,
-            genre_ids: show.genre_ids,
-            vote_average: show.vote_average,
-            overview: show.overview,
-            release_date: show.release_date,
-            first_air_date: show.first_air_date
-        }));
-
-        if (page === 1) {    
-            if (query) {
-                setShows([...shows, ...showsData]);
-            } else {
-                setShows(showsData);
-            }
-        } else {
-            if (query) {
-              
-                setShows(showsData);
-            } else {
-                setShows([...shows, ...showsData]);
-            }
-        }
-    };
-    console.log(query);
-    console.log(shows);
-    console.log(page);
-
-    useEffect(() => {
-        fetchData();
-    }, [sort, query, showType, page]);
 
     return <ShowContext.Provider value={providerValues}>{children}</ShowContext.Provider>;
 };
