f13842c14ec26535b94c9baf71ed4d8abfd26697
Marios G <splanis5@gmail.com>
2020-02-13 23:03:42 +0200

    fetch moved to Shows

--- src/components/pages/Shows/Shows.tsx
+++ src/components/pages/Shows/Shows.tsx
@@ -1,4 +1,4 @@
-import React, {useEffect} from "react";
+import React, { useEffect } from "react";
 import { useShows } from "../../context/ShowContext";
 import Show from "./Show";
 import Header from "../../Header/Header";
@@ -6,13 +6,69 @@ import styled from "styled-components";
 import BottomScrollListener from "react-bottom-scroll-listener";
 
 const Shows: React.FC = () => {
-    const { shows, page, setPage, query, setSort} = useShows();
+    const { shows, setShows, page, setPage, query, sort, showType, setSort } = useShows();
+
+    const API_KEY = `98b9ebfd32ac53d37febef32464f8607`;
+    const API_URL = `https://api.themoviedb.org/3/discover/${showType}?api_key=${API_KEY}&language=en-US&sort_by=${sort}&include_adult=false&include_video=false&page=${page}`;
+    const API_SEARCH_URL = `https://api.themoviedb.org/3/search/${showType}?api_key=${API_KEY}&language=en-US&query=${query}&include_adult=false&page=${page}`;
+    let FETCH_URL = "";
 
     const handlePage = () => {
         if (page !== 1000) {
             setPage(page + 1);
         }
     };
+
+    const fetchData = async () => {
+        if (query) {
+            FETCH_URL = API_SEARCH_URL;
+        } else {
+            FETCH_URL = API_URL;
+        }
+
+        const response = await fetch(FETCH_URL);
+        const fetchedShows = await response.json();
+
+        // const showsData = data.results.map((show: IShow) => ({
+        //     adult: show.adult,
+        //     backdrop_path: show.backdrop_path,
+        //     title: show.title,
+        //     id: show.id,
+        //     popularity: show.popularity,
+        //     vote_count: show.vote_count,
+        //     video: show.video,
+        //     poster_path: show.poster_path,
+        //     original_language: show.original_language,
+        //     original_name: show.original_name,
+        //     genre_ids: show.genre_ids,
+        //     vote_average: show.vote_average,
+        //     overview: show.overview,
+        //     release_date: show.release_date,
+        //     first_air_date: show.first_air_date
+        // }));
+
+        if (page === 1) {
+            setShows(fetchedShows.results);
+        } else {
+            setShows([...shows, ...fetchedShows.results]);
+        }
+    };
+
+    useEffect(() => {
+        fetchData();
+    }, [query]);
+
+    useEffect(() => {
+        if (page === 1) {
+            window.scrollTo(0, 0);
+        } else {
+            fetchData();
+        }
+    }, [page]);
+
+    useEffect(() => {
+        fetchData();
+    }, [sort, showType]);
 
     useEffect(() => {
         setSort("popularity.desc");
@@ -43,6 +99,7 @@ const Shows: React.FC = () => {
                     />
                 ))}
             </ShowsCards>
+            <ScrollDown>Scroll down to see more {showType === "movie" ? "movies" : "TV shows"}</ScrollDown>
             <BottomScrollListener onBottom={handlePage} />
         </ShowsContainer>
     );
@@ -53,13 +110,18 @@ const ShowsContainer = styled.div`
     flex-direction: column;
     justify-content: center;
     align-items: center;
-    padding: 5px;
+    margin: 15px;
 `;
 
 const ShowsCards = styled.div`
     display: flex;
     flex-wrap: wrap;
     justify-content: center;
+`;
+
+const ScrollDown = styled.p`
+    font-size: 1.4rem;
+    margin: 10px;
 `;
 
 export default Shows;
