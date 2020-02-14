61a10a950758fb757ff9646518a935f406d2c5d1
Marios G <splanis5@gmail.com>
2020-02-14 18:59:41 +0200

    Login/logout buttons added

--- src/components/layout/Navbar.tsx
+++ src/components/layout/Navbar.tsx
@@ -2,11 +2,19 @@ import React, { useEffect } from "react";
 import styled from "styled-components";
 import { Link } from "react-router-dom";
 import { useShows } from "../context/ShowContext";
+import { useUser } from "../context/UserContext";
+import { fireLogout } from "../../firebase/auth";
 
 const Movies: React.FC = () => {
+    const { user, setUser } = useUser();
     const { query, setQuery, setPage, setShowType } = useShows();
     const queryHandle = (e: React.ChangeEvent<HTMLInputElement>) => {
         setQuery(e.target.value);
+    };
+
+    const Logout = () => {
+        fireLogout();
+        setUser(null);
     };
 
     useEffect(() => {
@@ -44,7 +52,20 @@ const Movies: React.FC = () => {
                 >
                     Movies
                 </StyledLink>
-                <p style={{ fontSize: "1.5rem", margin: "20px" }}>Username</p>
+                {user ? (
+                    <Dropdown>
+                        <User>{user.username}</User>
+                        <DropdownItems>
+                            <DropDownButtons>Profile</DropDownButtons>
+                            <DropDownButtons onClick={Logout}>Logout</DropDownButtons>
+                        </DropdownItems>
+                    </Dropdown>
+                ) : (
+                    <Sign>
+                        <StyledLink to="/login">Login</StyledLink>
+                        <StyledLink to="/register">Register</StyledLink>
+                    </Sign>
+                )}
             </Links>
         </Nav>
     );
@@ -64,12 +85,14 @@ const Nav = styled.nav`
 `;
 
 const Logo = styled.div`
-    width: 250px;
+    width: 480px;
 `;
 
 const StyledLink = styled(Link)`
     color: white;
-    margin: 20px;
+    width: 160px;
+    display: flex;
+    justify-content: center;
     text-decoration: none;
     font-size: 1.5rem;
 `;
@@ -88,7 +111,55 @@ const Input = styled.input`
 const Links = styled.div`
     display: flex;
     align-items: center;
-    margin-right: 10px;
+`;
+
+const DropdownItems = styled.div`
+    position: absolute;
+    background: rgba(0, 0, 0, 0.8);
+    display: none;
+    flex-direction: column;
+    min-width: 160px;
+
+    box-shadow: 0px 8px 16px 0px rgba(0, 0, 0, 0.2);
+`;
+
+const Dropdown = styled.div`
+    position: relative;
+    display: inline-block;
+
+    &:hover {
+        ${DropdownItems} {
+            display: flex;
+        }
+    }
+`;
+
+const User = styled.p`
+    font-size: 1.5rem;
+    width: 160px;
+    height: 70px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    color: white;
+    z-index: 10;
+`;
+
+const DropDownButtons = styled.button`
+    font-size: 1.5rem;
+    border: none;
+    background: none;
+    color: white;
+    padding: 16px;
+    text-decoration: none;
+
+    &:hover {
+        cursor: pointer;
+    }
+`;
+
+const Sign = styled.div`
+    display: flex;
 `;
 
 export default Movies;
