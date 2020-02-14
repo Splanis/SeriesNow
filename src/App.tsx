bc9aa1269dd24496e5803647700e94fcc774fcda
Marios G <splanis5@gmail.com>
2020-02-14 18:59:27 +0200

    Added new paths

--- src/App.tsx
+++ src/App.tsx
@@ -4,35 +4,42 @@ import Homepage from "./components/pages/Homepage/Homepage";
 import Navbar from "./components/layout/Navbar";
 import Footer from "./components/layout/Footer";
 import Shows from "./components/pages/Shows/Shows";
+import Login from "./components/pages/Login/Login";
+import Register from "./components/pages/Register/Register";
 import Error404 from "./components/pages/Error404/Error404";
 import ShowDetail from "./components/pages/ShowDetail/ShowDetail";
 import styled from "styled-components";
 import GlobalStyles from "./components/sharedStyles/GlobalStyles";
 import { ShowProvider } from "./components/context/ShowContext";
 import { TrendingShowProvider } from "./components/context/TrendingShowContext";
+import { UserProvider } from "./components/context/UserContext";
 
 const App: React.FC = () => {
     return (
         <div>
-            <ShowProvider>
-                <TrendingShowProvider>
-                    <Router>
-                        <GlobalStyles />
-                        <Navbar />
-                        <Container>
-                            <Switch>
-                                <Route exact path="/" component={Homepage} />
-                                <Route exact path="/tv" component={Shows} />
-                                <Route exact path="/movie" component={Shows} />
-                                <Route exact path="/:showType/:id" component={ShowDetail} />
-                                <Route exact path="/:showType/:id" component={ShowDetail} />
-                                <Route component={Error404} />
-                            </Switch>
-                        </Container>
-                        <Footer />
-                    </Router>
-                </TrendingShowProvider>
-            </ShowProvider>
+            <UserProvider>
+                <ShowProvider>
+                    <TrendingShowProvider>
+                        <Router>
+                            <GlobalStyles />
+                            <Navbar />
+                            <Container>
+                                <Switch>
+                                    <Route exact path="/" component={Homepage} />
+                                    <Route exact path="/tv" component={Shows} />
+                                    <Route exact path="/movie" component={Shows} />
+                                    <Route exact path="/:showType/:id" component={ShowDetail} />
+                                    <Route exact path="/:showType/:id" component={ShowDetail} />
+                                    <Route exact path="/login" component={Login} />
+                                    <Route exact path="/register" component={Register} />
+                                    <Route component={Error404} />
+                                </Switch>
+                            </Container>
+                            <Footer />
+                        </Router>
+                    </TrendingShowProvider>
+                </ShowProvider>
+            </UserProvider>
         </div>
     );
 };
