c59a319f143db2b8adca1600777da1b50119e033
Marios G <splanis5@gmail.com>
2020-02-17 17:24:04 +0200

    handleSort removed

--- src/components/pages/Shows/header/Sort.tsx
+++ src/components/pages/Shows/header/Sort.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import styled from "styled-components";
 import { useShows } from "../../../context/ShowContext";
 import { Button } from "../../../sharedStyles/Button";
@@ -7,39 +7,44 @@ const Sort: React.FC = () => {
     const { setSort } = useShows();
     const [activeSort, setActiveSort] = useState<"popularity" | "newest" | "rating">("popularity");
 
-    const handleSort = (e: any) => {
-        switch (e.target.value) {
-            case "newest":
-                setSort("primary_release_date.desc");
-                setActiveSort("newest");
-                break;
-            case "rating":
-                setSort("vote_average.desc");
-                setActiveSort("rating");
-                break;
-            default:
-                setSort("popularity.desc");
-                setActiveSort("popularity");
-                break;
-        }
-    };
-
     return (
         <Buttons>
             Sort by
-            <Button style={{ background: activeSort === "popularity" ? "#333333" : "black" }} value="popularity" onClick={handleSort}>
+            <Button
+                style={{ background: activeSort === "popularity" ? "#333333" : "black" }}
+                onClick={() => {
+                    setSort("popularity.desc");
+                    setActiveSort("popularity");
+                }}
+            >
                 Popularity
             </Button>
-            <Button style={{ background: activeSort === "rating" ? "#333333" : "black" }} value="rating" onClick={handleSort}>
+            <Button
+                style={{ background: activeSort === "rating" ? "#333333" : "black" }}
+                onClick={() => {
+                    setSort("vote_average.desc");
+                    setActiveSort("rating");
+                }}
+            >
                 Rating
             </Button>
-            <Button style={{ background: activeSort === "newest" ? "#333333" : "black" }} value="newest" onClick={handleSort}>
+            <Button
+                style={{ background: activeSort === "newest" ? "#333333" : "black" }}
+                onClick={() => {
+                    setSort("primary_release_date.desc");
+                    setActiveSort("newest");
+                }}
+            >
                 Released: Newest
             </Button>
         </Buttons>
     );
 };
 
-const Buttons = styled.div``;
+const Buttons = styled.div`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+`;
 
 export default Sort;
