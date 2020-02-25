82b121ac0421d2df78239fbb314f5b174137a96a
Marios G <splanis5@gmail.com>
2020-02-26 00:47:53 +0200

    like-watchlist moved to seperate component

--- src/components/pages/Shows/Show.tsx
+++ src/components/pages/Shows/Show.tsx
@@ -1,25 +1,12 @@
-import React, { useState } from "react";
+import React from "react";
 import { IShow } from "../../context/ShowContext";
 import { useShows } from "../../context/ShowContext";
 import { Link } from "react-router-dom";
 import styled from "styled-components";
-import likeIcon from "../../../assets/icons/like.png";
-import likedIcon from "../../../assets/icons/liked.png";
-import watchlistIcon from "../../../assets/icons/watchlist.png";
-import addedWatchlistIcon from "../../../assets/icons/addedWatchlist.png";
+import LikeWatchlist from "../../shared/LikeWatchlist";
 
 const Show: React.FC<IShow> = ({ title, poster_path, overview, first_air_date, vote_average, id }) => {
     const { showType } = useShows();
-    const [liked, setLike] = useState<boolean>(false);
-    const [watchlist, setWatchlist] = useState<boolean>(false);
-
-    const handleLike = () => {
-        setLike(!liked);
-    };
-
-    const handleWatchlist = () => {
-        setWatchlist(!watchlist);
-    };
 
     return (
         <ShowCard>
@@ -41,14 +28,7 @@ const Show: React.FC<IShow> = ({ title, poster_path, overview, first_air_date, v
                         Rating:{" "}
                         <span style={{ color: vote_average >= 8 ? "green" : vote_average >= 5 ? "orange" : "red" }}>{vote_average}</span>
                     </Rating>
-                    <Buttons>
-                        <Button onClick={handleWatchlist}>
-                            <img src={watchlist ? addedWatchlistIcon : watchlistIcon} alt="" />
-                        </Button>
-                        <Button onClick={handleLike}>
-                            <img src={liked ? likedIcon : likeIcon} alt="" />
-                        </Button>
-                    </Buttons>
+                    <LikeWatchlist />
                 </Info>
             </ShowDetails>
         </ShowCard>
@@ -116,35 +96,12 @@ const Info = styled.div`
     align-items: center;
 `;
 
-const Buttons = styled.div`
-    display: flex;
-    justify-content: center;
-    align-items: center;
-`;
-
 const Rating = styled.p`
     font-weight: 600;
     font-size: 1.3rem;
 
     span {
         font-size: 1.3rem;
-    }
-`;
-
-const Button = styled.button`
-    border: none;
-    background: none;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    margin: 0 15px;
-
-    img {
-        height: 30px;
-
-        &:hover {
-            cursor: pointer;
-        }
     }
 `;
 
